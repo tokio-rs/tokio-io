30e4846a8b1df470725cec9420c97bf1f1f64e5f
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-03-18 10:28:12 +0100

    Fix typo in codec module docs

--- src/codec.rs
+++ src/codec.rs
@@ -1,6 +1,6 @@
 //! Utilities for encoding and decoding frames.
 //!
-//! Contains adapters to go from strams of bytes, [`AsyncRead`] and
+//! Contains adapters to go from streams of bytes, [`AsyncRead`] and
 //! [`AsyncWrite`], to framed streams implementing [`Sink`] and [`Stream`].
 //! Framed streams are also known as [transports].
 //!
